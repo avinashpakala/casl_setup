6b6983a88fc5a3a83ce4ab5e414fd3483e12abc7
“girirajthara” <prasad.1@iitj.ac.in>
2020-12-31 12:07:16 +0530

    setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginForm } from './login.component';
+import { TestComponent } from './test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component:LoginForm
+  },
+  {
+    path:'test',
+    component:TestComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AbilityModule } from '@casl/angular';
+import { Ability, PureAbility } from '@casl/ability';
+import { FormsModule } from '@angular/forms';
+import { LoginForm } from './login.component';
+import { TestComponent } from './test.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginForm,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    AbilityModule
   ],
-  providers: [],
+  providers: [
+    { provide: Ability, useValue: new Ability() },
+    { provide: PureAbility, useExisting: Ability }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
