0917a04f395c04a8398493c2f2c8961964110853
Avinash <avinash@heresy.in>
2020-12-31 12:59:39 +0530

    json ability

--- src/app/test.servce.ts
+++ src/app/test.servce.ts
@@ -4,23 +4,36 @@ import { Ability, AbilityBuilder } from '@casl/ability';
 
 @Injectable({ providedIn: 'root' })
 export class Session {
-  public token: string= ''
+  public token: string = ''
 
-  constructor(private ability: Ability) {}
+  constructor(private ability: Ability) { }
 
   login() {
-    this.updateAbility('admindsddsdff');
+    this.updateAbility('admin');
     this.token = "session.token";
-    
+
   }
 
-  private updateAbility(user:string) {
-    const { can, rules } = new AbilityBuilder<Ability>();
+  private updateAbility(user: string) {
+    // const { can, rules } = new AbilityBuilder<Ability>();
 
-    if (user === 'admin') {
-      can('manage', 'all');
-    } else {
-      can('read', 'all');
+    // if (user === 'admin') {
+    //   can('manage', 'all');
+    // } else {
+    //   can('read', 'all');
+    // }
+    var rules;
+    if (user == 'admin') {
+      rules = [{
+        action: "manage",
+        subject: "all"
+      }];
+    }
+    else {
+      rules = [{
+        action: "read",
+        subject: "all"
+      }];
     }
 
     this.ability.update(rules);
